631ede019193c58d444fa71a5cd61ce4c6b24195
Miguel A. M <engmiguelmonteiro@gmail.com>
2022-09-14 21:20:40 -0300

    Adicionado Toastfy

--- escolareact/src/routes/index.js
+++ escolareact/src/routes/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import { Switch } from 'react-router-dom';
-import { toast } from 'react-toastify';
 
 import MyRoute from './MyRoute';
 import Login from '../pages/login';
 import Page404 from '../pages/page404';
 
 export default function Routes() {
-  toast.success('Sucesso!');
-  toast.error('Erro!');
   return (
     <Switch>
       <MyRoute exact path="/" component={Login} />
